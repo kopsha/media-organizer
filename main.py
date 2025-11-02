63b77d92aeb8a071dc3c6d64ed86be9110b0c38b
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2025-11-02 21:50:20 +0200

    Clean, maybe group by location and tyme

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from datetime import datetime, timezone
 from mimetypes import guess_type
 from pathlib import Path
 from pprint import pprint
+from uuid import uuid4
 
 import exifread
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.geocoders import Nominatim
 
 geolocator = RateLimiter(
-    Nominatim(user_agent="photo-location").reverse, min_delay_seconds=1/10
+    Nominatim(user_agent="photo-location").reverse, min_delay_seconds=1 / 10
 )
 
 
@@ -41,11 +42,11 @@ def dms_to_decimal(dms, ref):
     return decimal
 
 
-def from_exif_info(exif_data: dict) -> dict:
-    if not exif_data:
-        return {}
-
+def cherrypick_image_properties(exif_data: dict) -> dict:
     data = dict()
+
+    if not exif_data:
+        return data
 
     if original_time := str(exif_data.get("EXIF DateTimeOriginal")):
         timestamp = datetime.strptime(original_time, "%Y:%m:%d %H:%M:%S")
@@ -65,102 +66,39 @@ def from_exif_info(exif_data: dict) -> dict:
 
 
 def scan_image(filepath: Path) -> dict:
-    with open(filepath, "rb") as image_file:
-        exif_data = exifread.process_file(image_file, details=False)
-        image_data = from_exif_info(exif_data)
-
     return image_data
 
 
-def get_image_date_taken(filepath: Path):
-    """Extracts the date taken from image metadata."""
-    result = None
-    try:
-        # Attempt to read exif with pillow
-        image = Image.open(filepath)
-        exif_data = image._getexif() if hasattr(image, "_getexif") else dict()
-        pprint(exif_data)
-        for tag_id, value in exif_data.items():
-            tag_name = TAGS.get(tag_id, tag_id)
-            if tag_name == "DateTimeOriginal":
-                result = datetime.strptime(value, "%Y:%m:%d %H:%M:%S")
-                break
-        else:
-            raise ValueError(f"{filepath} has no exif data.")
-    except Exception:
-        timestamp = filepath.stat().st_mtime
-        result = datetime.fromtimestamp(timestamp)
-    print(".", end="")
-    return result
-
-
-def organize_images_by_date(source: Path, destination: Path, use_copy: bool):
+def scan_all_media_files(source: Path, destination: Path, use_copy: bool):
     """Organizes images into folders by year/month/day."""
-    file_counter = 0
-    media_types_trans = dict(
-        image="poze",
-        video="video",
-        audio="muzica",
-    )
-    seen = set()
-    for src_filepath in source.rglob("*"):
-        if file_counter > 50:
-            break
+    db = dict()
 
+    for src_filepath in source.rglob("IMG_*"):
         if src_filepath.is_file():
             try:
-                full_mime_type, _ = guess_type(src_filepath)
-                print("---", src_filepath, "/", full_mime_type, "---")
-                if not full_mime_type:
+                mime_type, _ = guess_type(src_filepath)
+                print("---", src_filepath.name, "/", mime_type, "---")
+
+                if not mime_type:
                     continue
 
-                if full_mime_type.startswith("image"):
-                    data = scan_image(src_filepath)
-                    print(data)
+                uid = uuid4()
+                properties = dict()
 
-                # date_taken = get_image_date_taken(src_filepath)
-                # year = date_taken.strftime("%Y")
-                # month = date_taken.strftime("%m - %B")
-                #
-                # previous_name = src_filepath.parent.name.strip()
-                # day_prefix = f"{date_taken.day:02} - "
-                # if previous_name.startswith(day_prefix):
-                #     previous_name = previous_name[len(day_prefix) :]
-                #
-                # day_meta = "{day:02} - {name}".format(
-                #     day=date_taken.day, name=previous_name
-                # )
-                #
-                # media_type = (full_mime_type or "/").split("/")[0]
-                # category = media_types_trans.get(media_type, "altele")
-                #
-                # # prepare target folder
-                # if category == "muzica":
-                #     dst_folder = destination / category
-                # else:
-                #     dst_folder = destination / category / year / month / day_meta
-                #
-                # if dst_folder not in seen:
-                #     dst_folder.mkdir(parents=True, exist_ok=True)
-                #     seen.add(dst_folder)
-                #
-                # # move/copy target file
-                # dst_filepath = dst_folder / src_filepath.name
-                #
-                # if dst_filepath.exists():
-                #     print("skip", dst_filepath, "already exists.")
-                #     continue
+                if mime_type.startswith("image"):
+                    with open(src_filepath, "rb") as image_file:
+                        exif_data = exifread.process_file(image_file, details=False)
+                        image_props = cherrypick_image_properties(exif_data)
+                        properties.update(image_props)
 
-                file_counter += 1
-                # if use_copy:
-                #     shutil.copy(src_filepath, dst_filepath)
-                # else:
-                #     shutil.move(src_filepath, dst_filepath)
+                properties["original_filename"] = src_filepath.stem
+                properties["original_extension"] = src_filepath.suffix
+                db[uid] = properties
 
             except Exception as err:
                 print(f"Cannot process {src_filepath}, reason: {err}")
 
-    return file_counter, len(seen)
+    return db
 
 
 if __name__ == "__main__":
@@ -181,6 +119,10 @@ if __name__ == "__main__":
     elif source == destination:
         parser.error(f"Source and destination cannot be the same ({source})")
 
-    files, folders = organize_images_by_date(source, destination, args.copy)
+    files = scan_all_media_files(source, destination, args.copy)
     print()
-    print(f"Organized {files} files in {folders} folders.")
+
+    for uid, props in files.items():
+        print(f"{uid}", props)
+
+    print(f"Found {len(files)} files.")
